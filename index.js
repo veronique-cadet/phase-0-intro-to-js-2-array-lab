f958d743a5a3e088c3062e0839cdccd2d709c21c
Veronique Cadet <cadetvero@gmail.com>
2022-11-06 23:42:00 +0100

    commit tree 88470632bed03fc36334166cd85ab06829b55615

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+function destructivelyRemoveLastCat(name) {
+  cats.pop(name);
+  return cats;
+}
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name);
+  return cats;
+}
+function appendCat(name) {
+  const newArray = cats.slice();
+  newArray.push(name);
+  return newArray;
+}
+function prependCat(name) {
+  const newArray = cats.slice();
+  newArray.unshift(name);
+  return newArray;
+}
+function removeLastCat(name) {
+  const newArray = cats.slice();
+  newArray.pop(name);
+  return newArray;
+}
+function removeFirstCat(name) {
+  const newArray = cats.slice();
+  newArray.shift(name);
+  return newArray;
+}
